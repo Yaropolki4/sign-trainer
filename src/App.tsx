1a33a8d2dd568a80afda041571e0eece8f8ea47e
yaroslav pisarev <yaroslav@yaroslavs-MacBook-Pro.local>
2024-12-18 16:26:20 +0300

    add-tailwind-prettier-plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 const App = () => {
   return (
-    <div>
-      <h1 className="text-primary text-4xl font-bold">Hello, React!</h1>
-    </div>
+    <a
+      href="#"
+      className="group mx-auto block max-w-xs space-y-3 rounded-lg bg-white p-6 shadow-lg ring-1 ring-slate-900/5 hover:bg-sky-500 hover:ring-sky-500"
+    >
+      <div className="flex items-center space-x-3">
+        <h3 className="text-sm font-semibold text-slate-900 group-hover:text-white">
+          New project
+        </h3>
+      </div>
+      <p className="text-sm text-slate-500 group-hover:text-white">
+        Create a new project from a variety of starting templates.
+      </p>
+    </a>
   );
 };
 
